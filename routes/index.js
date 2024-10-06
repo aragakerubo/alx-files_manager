e741aa8f37973ef772a78e52b2e53599b9ac4382
aragakerubo <aragakerubo@gmail.com>
2024-10-05 23:41:20 -0700

    4. Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+import express from "express";
+import AppController from "../controllers/AppController";
+import UsersController from "../controllers/UsersController";
+import AuthController from "../controllers/AuthController";
 
 const router = express.Router();
 
 // Define routes
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
+router.get("/status", AppController.getStatus);
+router.get("/stats", AppController.getStats);
+router.post("/users", UsersController.postNew);
+router.get("/connect", AuthController.getConnect); // Login endpoint
+router.get("/disconnect", AuthController.getDisconnect); // Logout endpoint
+router.get("/users/me", UsersController.getMe); // Get current user endpoint
 
 // Export the router so it can be used in the server
 export default router;
--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,13 @@
 // DB Class
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from "mongodb";
 
 const { DB_HOST, DB_PORT, DB_DATABASE } = {
-  // DB host
-  DB_HOST: process.env.DB_HOST || 'localhost',
-  // DB port
-  DB_PORT: process.env.DB_PORT || 27017,
-  // DB name
-  DB_DATABASE: process.env.DB_DATABASE || 'files_manager',
+    // DB host
+    DB_HOST: process.env.DB_HOST || "localhost",
+    // DB port
+    DB_PORT: process.env.DB_PORT || 27017,
+    // DB name
+    DB_DATABASE: process.env.DB_DATABASE || "files_manager",
 };
 
 /**
@@ -20,81 +20,94 @@ const { DB_HOST, DB_PORT, DB_DATABASE } = {
  * @method createUser(user) Create a new user
  */
 class DBClient {
-  constructor() {
-    // Create a new MongoClient
-    this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, {
-      useUnifiedTopology: true,
-    });
+    constructor() {
+        // Create a new MongoClient
+        this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, {
+            useUnifiedTopology: true,
+        });
 
-    // Create a placeholder for the database
-    this.db = null;
-
-    // Handle connection to the database
-    (async () => {
-      try {
-        await this.client.connect(); // Await the connection
-        this.db = this.client.db(DB_DATABASE);
-        console.log(
-          `Connection to DB established on ${DB_HOST}:${DB_PORT}`,
-        );
-      } catch (error) {
-        console.error(`MongoDB connection error: ${error.message}`);
+        // Create a placeholder for the database
         this.db = null;
-      }
-    })();
-  }
 
-  // Check if the connection is established
-  isAlive() {
-    return (
-      this.client.topology
-            && this.client.topology.isConnected()
-            && this.db !== null
-    );
-  }
-
-  // Get the number of documents in the users collection
-  async nbUsers() {
-    if (!this.db) return 0;
-    try {
-      return await this.db.collection('users').countDocuments();
-    } catch (error) {
-      console.error(`Error fetching users count: ${error.message}`);
-      return 0;
+        // Handle connection to the database
+        (async () => {
+            try {
+                await this.client.connect(); // Await the connection
+                this.db = this.client.db(DB_DATABASE);
+                console.log(
+                    `Connection to DB established on ${DB_HOST}:${DB_PORT}`
+                );
+            } catch (error) {
+                console.error(`MongoDB connection error: ${error.message}`);
+                this.db = null;
+            }
+        })();
     }
-  }
 
-  // Get the number of documents in the files collection
-  async nbFiles() {
-    if (!this.db) return 0;
-    try {
-      return await this.db.collection('files').countDocuments();
-    } catch (error) {
-      console.error(`Error fetching files count: ${error.message}`);
-      return 0;
+    // Check if the connection is established
+    isAlive() {
+        return (
+            this.client.topology &&
+            this.client.topology.isConnected() &&
+            this.db !== null
+        );
     }
-  }
 
-  async getUserByEmail(email) {
-    if (!this.db) return 0;
-    try {
-      return await this.db.collection('users').findOne({ email });
-    } catch (error) {
-      console.error(`Error fetching user by email: ${error.message}`);
-      return 0;
+    // Get the number of documents in the users collection
+    async nbUsers() {
+        if (!this.db) return 0;
+        try {
+            return await this.db.collection("users").countDocuments();
+        } catch (error) {
+            console.error(`Error fetching users count: ${error.message}`);
+            return 0;
+        }
     }
-  }
 
-  async createUser(user) {
-    if (!this.db) return 0;
-    try {
-      const result = await this.db.collection('users').insertOne(user);
-      return { _id: result.ops[0]._id, email: result.ops[0].email };
-    } catch (error) {
-      console.error(`Error creating user: ${error.message}`);
-      return 0;
+    // Get the number of documents in the files collection
+    async nbFiles() {
+        if (!this.db) return 0;
+        try {
+            return await this.db.collection("files").countDocuments();
+        } catch (error) {
+            console.error(`Error fetching files count: ${error.message}`);
+            return 0;
+        }
     }
-  }
+
+    async getUserByEmail(email) {
+        if (!this.db) return 0;
+        try {
+            return await this.db.collection("users").findOne({ email });
+        } catch (error) {
+            console.error(`Error fetching user by email: ${error.message}`);
+            return 0;
+        }
+    }
+
+    async createUser(user) {
+        if (!this.db) return 0;
+        try {
+            const result = await this.db.collection("users").insertOne(user);
+            return result.ops[0];
+        } catch (error) {
+            console.error(`Error creating user: ${error.message}`);
+            return 0;
+        }
+    }
+
+    // Get user by id
+    async getUserById(userId) {
+        if (!this.db) return 0;
+        try {
+            return await this.db
+                .collection("users")
+                .findOne({ _id: new ObjectId(userId) });
+        } catch (error) {
+            console.error(`Error fetching user by id: ${error.message}`);
+            return 0;
+        }
+    }
 }
 
 const dbClient = new DBClient();
