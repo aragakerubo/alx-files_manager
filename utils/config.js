fad37c464a3db14581aa852fa8b58a33c45e7c1f
aragakerubo <aragakerubo@gmail.com>
2024-10-05 19:27:18 -0700

    1. MongoDB utils

--- utils/config.js
+++
@@ -1,9 +0,0 @@
-// Configurations for the application
-export default {
-    // DB host
-    DB_HOST: process.env.DB_HOST || "localhost",
-    // DB port
-    DB_PORT: process.env.DB_PORT || 27017,
-    // DB name
-    DB_DATABASE: process.env.DB_DATABASE || "files_manager",
-};
--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,14 @@
 // DB Class
-import { MongoClient } from "mongodb";
-import { DB_HOST, DB_PORT, DB_DATABASE } from "./config";
+import { MongoClient } from 'mongodb';
+
+const { DB_HOST, DB_PORT, DB_DATABASE } = {
+  // DB host
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  // DB port
+  DB_PORT: process.env.DB_PORT || 27017,
+  // DB name
+  DB_DATABASE: process.env.DB_DATABASE || 'files_manager',
+};
 
 /**
  * DBClient Class
@@ -10,60 +18,60 @@ import { DB_HOST, DB_PORT, DB_DATABASE } from "./config";
  * @method nbFiles() Get the number of documents in the files collection
  */
 class DBClient {
-    constructor() {
-        // Create a new MongoClient
-        this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, {
-            useUnifiedTopology: true,
-        });
+  constructor() {
+    // Create a new MongoClient
+    this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, {
+      useUnifiedTopology: true,
+    });
 
-        // Create a placeholder for the database
-        this.db = null;
+    // Create a placeholder for the database
+    this.db = null;
 
-        // Handle connection to the database
-        (async () => {
-            try {
-                await this.client.connect(); // Await the connection
-                this.db = this.client.db(DB_DATABASE);
-                console.log(
-                    `Connection to DB established on ${DB_HOST}:${DB_PORT}`
-                );
-            } catch (error) {
-                console.error(`MongoDB connection error: ${error.message}`);
-                this.db = null;
-            }
-        })();
-    }
-
-    // Check if the connection is established
-    isAlive() {
-        return (
-            this.client.topology &&
-            this.client.topology.isConnected() &&
-            this.db !== null
+    // Handle connection to the database
+    (async () => {
+      try {
+        await this.client.connect(); // Await the connection
+        this.db = this.client.db(DB_DATABASE);
+        console.log(
+          `Connection to DB established on ${DB_HOST}:${DB_PORT}`,
         );
-    }
+      } catch (error) {
+        console.error(`MongoDB connection error: ${error.message}`);
+        this.db = null;
+      }
+    })();
+  }
 
-    // Get the number of documents in the users collection
-    async nbUsers() {
-        if (!this.db) return 0;
-        try {
-            return await this.db.collection("users").countDocuments();
-        } catch (error) {
-            console.error(`Error fetching users count: ${error.message}`);
-            return 0;
-        }
-    }
+  // Check if the connection is established
+  isAlive() {
+    return (
+      this.client.topology
+            && this.client.topology.isConnected()
+            && this.db !== null
+    );
+  }
 
-    // Get the number of documents in the files collection
-    async nbFiles() {
-        if (!this.db) return 0;
-        try {
-            return await this.db.collection("files").countDocuments();
-        } catch (error) {
-            console.error(`Error fetching files count: ${error.message}`);
-            return 0;
-        }
+  // Get the number of documents in the users collection
+  async nbUsers() {
+    if (!this.db) return 0;
+    try {
+      return await this.db.collection('users').countDocuments();
+    } catch (error) {
+      console.error(`Error fetching users count: ${error.message}`);
+      return 0;
     }
+  }
+
+  // Get the number of documents in the files collection
+  async nbFiles() {
+    if (!this.db) return 0;
+    try {
+      return await this.db.collection('files').countDocuments();
+    } catch (error) {
+      console.error(`Error fetching files count: ${error.message}`);
+      return 0;
+    }
+  }
 }
 
 const dbClient = new DBClient();
